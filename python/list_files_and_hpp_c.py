a345af748bbdbbcc0293a11eaeb32600a2c01a63
Giddius <GordonGrossmann@gmx.at>
2020-05-02 13:54:59 +0200

    Update list_files_and_hpp_c.py

--- python/list_files_and_hpp_c.py
+++ python/list_files_and_hpp_c.py
@@ -1,8 +1,14 @@
+#region imports
 import os
 import pprint
 import pandas as pd
+import armaclass
+from collections import OrderedDict
+import sqlite3
+#endregion
 
 
+#region baseclass
 class base_mission_filesearch:
     """File handling for searching mission folders for sqf and hpp files"""
 
@@ -27,6 +33,8 @@ class base_mission_filesearch:
             self.filefullpathdict = {}
         else:
             self.filefullpathdict = filefullpathdict
+
+
 
     def printfilefullpathdict(self):
         pprint.pprint(self.filefullpathdict)
@@ -56,14 +64,45 @@ class base_mission_filesearch:
         if '/' in PATH:
             return PATH.replace('/', '\\')
 
+    @staticmethod
+    def create_tables():
+        conn = sqlite3.connect('AS_phonebook.db')
+        c = conn.cursor()
+        c.execute("""CREATE TABLE fnchpp_tbl (
+                fnchpp_ID integer,
+                fnchpp_path text,
+                fnchpp_file text
+                )""")
+
+        c.execute("""CREATE TABLE sqf_tbl (
+                sqf_ID integer,
+                sqf_path text,
+                sqf_file text
+                )""")
+
+        c.execute("""CREATE TABLE fnc_tbl (
+                fnc_ID integer,
+                fnc_path text,
+                fnc_file text,
+                fnc_callname text
+                )""")
+
+        c.execute("""CREATE TABLE call_list_tbl (
+                sqf_ID integer,
+                fnc_ID integer
+                )""")
+
+        conn.commit()
+        conn.close()
+
+
     def walkthefile(self):
-        self.initOutputfile()
         for roots, directories, files in os.walk(self.pybasefolder):
             for file in files:
                 if self.type in file:
                     roots = roots.replace('\\', '/')
                     roots = roots.replace('//', '/')
-                    self.appendoutput('%s,%s,%s\\%s,%s\n' % (self.curID, self.normpath(roots), self.normpath(roots), file, file))
+                    self.insert_upperdata(self.curID, self.normpath(roots), file)
                     self.filefolderdict[self.curID] = roots
                     self.filefullpathdict[self.curID] = str(roots + '/' + file)
                     self.filenamedict[self.curID] = file
@@ -79,15 +118,64 @@ class base_mission_filesearch:
         with open(self.filefullpathdict[KEY],'r') as f:
             return f.read()
 
+    @staticmethod
+    def callerdatain(cID, tID):
+        conn = sqlite3.connect('AS_phonebook.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO call_list_tbl VALUES (?, ?)", (cID, tID))
+        conn.commit()
+        conn.close()
+
+
+#endregion
+
+#region class for functions.hpp files
 class functions_hpp(base_mission_filesearch):
 
-    def __init__(self, outfile, filenamedict=None, filefolderdict=None, filefullpathdict=None):
+    def __init__(self, outfile, filenamedict=None, filefolderdict=None, filefullpathdict=None, functionsdict=None):
 
         super().__init__(outfile, type, filenamedict=None, filefolderdict=None, filefullpathdict=None)
         self.type = 'functions.hpp'
+        if functionsdict is None:
+            self.functionsdict = {}
+        else:
+            self.functionsdict = functionsdict
+
+    def insert_upperdata(self, ID, PATH, FILE):
+        conn = sqlite3.connect('AS_phonebook.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO fnchpp_tbl VALUES (?, ?, ?)", (ID, PATH, FILE))
+        conn.commit()
+        conn.close()
+
+    def insert_lowerdata(self, ID, PATH, FILE, CALLNAME):
+        conn = sqlite3.connect('AS_phonebook.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO fnc_tbl VALUES (?, ?, ?, ?)", (ID, PATH, FILE, CALLNAME))
+        conn.commit()
+        conn.close()
+
+    def getfunctions(self):
+        fncID = 0
+        ASfncFolder = os.path.join(baseFolder, 'functions')
+        for i in range(len(self.filefullpathdict)):
+            test = armaclass.parse(self.getcontent(i))
+            for prefix, value in test.items():
+                for folder, belue in value.items():
+                    for function in belue:
+                        path = os.path.join(ASfncFolder, folder)
+                        funct = prefix + '_fnc_' + function
+                        functfile = 'fn_' + function + '.sqf'
+                        pathfull = os.path.join(ASfncFolder, folder, functfile)
+                        self.insert_lowerdata(fncID, path, functfile, funct)
+                        self.functionsdict[fncID] = funct
+                        fncID = fncID + 1
 
 
 
+#endregion
+
+#region class for sqf files
 class sqf_files(base_mission_filesearch):
 
     def __init__(self, outfile, filenamedict=None, filefolderdict=None, filefullpathdict=None):
@@ -95,7 +183,19 @@ class sqf_files(base_mission_filesearch):
         super().__init__( outfile, type, filenamedict=None, filefolderdict=None, filefullpathdict=None)
         self.type = '.sqf'
 
+    def insert_upperdata(self, ID, PATH, FILE):
+        conn = sqlite3.connect('AS_phonebook.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO sqf_tbl VALUES (?, ?, ?)", (ID, PATH, FILE))
+        conn.commit()
+        conn.close()
 
+
+
+
+#endregion
+
+#region external in and Outputs, is temporary
 cwd = os.getcwd()
 subfol = 'python'
 baseFolder = 'D:\Dropbox\hobby\Modding\Programs\Github\Foreign_Repos\A3-Antistasi'
@@ -103,25 +203,34 @@ outputFolderName = 'CSV_Output'
 sqfOutputFileName = 'Mission_File_list_SQF.csv'
 fncOutputFileName = 'Mission_File_list_Fnc.csv'
 functemppath = os.path.join(subfol, outputFolderName, 'Output.csv')
+#endregion
 
-fnc = functions_hpp(fncOutputFileName)
-sqf = sqf_files(sqfOutputFileName)
 
-fnc.walkthefile()
-sqf.walkthefile()
 
-fncData_df = pd.read_csv(
-    functemppath,      # relative python path to subdirectory
-    sep=',',
-    dtype={'fnc_As_ID': int,'fnc_As_path': str,'fnc_As_filename': str,'fnc_As_name': str},
-    usecols=['fnc_As_ID','fnc_As_name'],   # Only load the three columns specified.
-    na_values=['.', '??']       # Take any '.' or '??' values as NA
-)
-FncDict = {}
-for ind in fncData_df.index:
-    FncDict[fncData_df['fnc_As_ID'][ind]] = fncData_df['fnc_As_name'][ind]
 
-for i in range(len(sqf.filefullpathdict)):
-    for ida, namea in FncDict.items():
-        if namea in sqf.getcontent(i):
-            pprint.pprint('{},{}'.format(i,ida))
+
+
+def initialize_db():
+    base_mission_filesearch.create_tables()
+    fnc = functions_hpp(fncOutputFileName)
+    sqf = sqf_files(sqfOutputFileName)
+    fnc.walkthefile()
+    sqf.walkthefile()
+    fnc.getfunctions()
+
+
+    for i in range(len(sqf.filefullpathdict)):
+        content = sqf.getcontent(i)
+        for nu in range(len(fnc.functionsdict)):
+            func = fnc.functionsdict[nu]
+            if func in content:
+                base_mission_filesearch.callerdatain(i, nu)
+
+
+
+initialize_db()
+print('done!')
+
+#TODO: change written paths to relative
+#TODO: change csv to sql tables
+#TODO: put function names in dict with ID for searching
